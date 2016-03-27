7d89034ea9740d6c28e1220eb2bd672607777819
stefan <sjsafranek@gmail.com>
2016-03-27 11:21:54 -0700

    change Conf

--- WallOfDenial/Conf.py
+++ WallOfDenial/Conf.py
@@ -1,17 +1,6 @@
 
-app_config = {
-    "gis": {
-    	"server": "http://localhost:8888",
-        "apikey": "7q1qcqmsxnvw"
-    },
-    "ml": {
-        "server": "http://ips.colab.duke.edu:8888",
-        "apikey": None
-    }
-}
-
 # gis_default_url = "http://localhost:8888"
 gis_default_key = "7q1qcqmsxnvw"
-gis_default_url = "http://52.18.218.56:8888"
-ml_default_url = "http://ips.colab.duke.edu:8023"
+gis_default_url = "https://gis.internalpositioning.com"
+ml_default_url = "https://ml.internalpositioning.com"
 
